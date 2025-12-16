8b8d89856d6844c887f550c196ae3aa935c1d916
a.kozyura <a.kozyura@it-is.ru>
2025-12-16 19:12:03 +0300

    add frinds query

--- src/friends/friends.controller.ts
+++ src/friends/friends.controller.ts
@@ -1,17 +1,25 @@
 import { Controller, Delete, Param, ParseIntPipe, Put } from '@nestjs/common';
 import { FriendsService } from './friends.service';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { UserResponse } from 'src/users/dto/index.response';
 
 @Controller('friend')
 export class FriendsController {
   constructor(private readonly friendService: FriendsService) {}
 
   @Put('set/:id')
-  async addFriend(@Param('id', ParseIntPipe) id: number) {
-    return await this.friendService.addFriend();
+  async addFriend(
+    @CurrentUser() user: UserResponse,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return await this.friendService.addFriend(user.id, id);
   }
 
   @Delete('delete/:id')
-  async deleteFriend(@Param('id', ParseIntPipe) id: number) {
-    return await this.friendService.deleteFriend();
+  async deleteFriend(
+    @CurrentUser() user: UserResponse,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return await this.friendService.deleteFriend(user.id, id);
   }
 }
--- src/friends/friends.service.ts
+++ src/friends/friends.service.ts
@@ -1,6 +1,7 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Pool } from 'pg';
+import { query } from 'src/utils/query';
 
 @Injectable()
 export class FriendsService {
@@ -10,7 +11,41 @@ export class FriendsService {
     private jwtService: JwtService,
   ) {}
 
-  async addFriend(id: number) {}
+  async addFriend(current_user_id: number, friend_id: number) {
+    if (current_user_id === friend_id) {
+      throw new BadRequestException();
+    }
 
-  async deleteFriend(id: number) {}
+    try {
+      await query(
+        this.poolMaster,
+        `INSERT INTO friends(user_id, friend_id)
+        VALUES ($1, $2)`,
+        [current_user_id, friend_id],
+      );
+      return 'Успешно создан пост';
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
+
+  async deleteFriend(current_user_id: number, friend_id: number) {
+    if (current_user_id === friend_id) {
+      throw new BadRequestException();
+    }
+
+    try {
+      await query(
+        this.poolMaster,
+        `DELETE FROM friends where 
+         current_user_id = $1 and  friend_id = $2)`,
+        [current_user_id, friend_id],
+      );
+      return 'Успешно создан пост';
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
+    }
+  }
 }
